287dbd99aca3c7952df9aa3dac59b79f4f4771d5
Dina Orlova <dina.turilova@gmail.com>
2021-02-11 10:57:53 +0300

    added tests

---
+++ src/test/java/ru/netology/web/test/RegistrationTest.java
@@ -0,0 +1,59 @@
+package ru.netology.web.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataGenerator;
+import ru.netology.web.data.RegistrationInfo;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    void shouldTestActiveUser() {
+        RegistrationInfo registrationInfo = DataGenerator.Registration.generateActiveUser();
+        DataGenerator.UserCreation.setUpAll(registrationInfo);
+        $("[data-test-id=login] input").setValue(registrationInfo.getLogin());
+        $("[data-test-id=password] input").setValue(registrationInfo.getPassword());
+        $("[data-test-id=action-login]").click();
+        $(withText("Личный кабинет")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldTestBlockedUser() {
+        RegistrationInfo registrationInfo = DataGenerator.Registration.generateBlockedUser();
+        DataGenerator.UserCreation.setUpAll(registrationInfo);
+        $("[data-test-id=login] input").setValue(registrationInfo.getLogin());
+        $("[data-test-id=password] input").setValue(registrationInfo.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification]").shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldGetErrorWhenInvalidLogin() {
+        RegistrationInfo registrationInfo = DataGenerator.Registration.generateActiveUser();
+        DataGenerator.UserCreation.setUpAll(registrationInfo);
+        $("[data-test-id=login] input").setValue("Dima-Kruglov");
+        $("[data-test-id=password] input").setValue(registrationInfo.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification]").shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldGetErrorWhenInvalidPassword() {
+        RegistrationInfo registrationInfo = DataGenerator.Registration.generateActiveUser();
+        DataGenerator.UserCreation.setUpAll(registrationInfo);
+        $("[data-test-id=login] input").setValue(registrationInfo.getLogin());
+        $("[data-test-id=password] input").setValue("vvvvv");
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification]").shouldBe(Condition.visible);
+    }
+}
